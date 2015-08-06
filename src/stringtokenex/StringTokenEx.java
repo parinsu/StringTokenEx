ad1a557630aa19d32e54cfc4610e58dfd6e31bd3
parinsu <parinsu@parinsu-TravelMate-8372>
2015-08-06 17:11:32 +0700

    Second Test Delete unnescessary comments

--- src/stringtokenex/StringTokenEx.java
+++ src/stringtokenex/StringTokenEx.java
@@ -1,23 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package stringtokenex;
 
 import java.util.StringTokenizer;
 
-/**
- *
- * @author parinsu
- */
+
 public class StringTokenEx {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         String delims = ",";
         String splitString = "one,two,,three,four,,five";
         
